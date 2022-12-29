1a33a917e396ef62af9ea1575ba24d28cd228a42
Grant T. Olson <kgo@grant-olson.net>
2022-12-29 11:12:10 -0500

    Isolate top module from i2c_target to make testbenches easier

--- src/i2c_target.v
+++ src/i2c_target.v
@@ -210,31 +210,3 @@ module i2c_target
 
 endmodule // i2c
 
-module top
-  (
-   input clk_i,
-   input rst_ni,
-   input scl_i,
-   inout sda_io,
-
-   output dbg_start_o,
-   output [3:0] dbg_state_o
-   
-   );
-
-   i2c_target ic2(
-                  .clk_i(clk_i),
-                  .rst_ni(rst_ni),
-                  .assigned_address_i(7'b1110000),
-                  .scl_i(scl_i),
-                  .sda_io(sda_io),
-                  // To track state with a hardware logic analyzer
-                  // for debugging. Not needed when not debugging
-                  .dbg_start_o(dbg_start_o),
-                  .dbg_state_o(dbg_state_o)
-                  );
-   
-                  
-endmodule // top
-
-
