ce0f8a80a8cf016befb5358ecbb1ef5dfb6b8f75
Grant T. Olson <kgo@grant-olson.net>
2023-01-10 23:47:12 -0500

    Use +: syntax to make things a little easier to read

--- src/top_module.v
+++ src/top_module.v
@@ -50,6 +50,7 @@ module top
    reg [0:15]            led_oe_setting_r;
    
    wire [0:15]    pwm_w;
+   
 
    assign led_o = oe_ni ? led_oe_setting_r : pwm_w;
 
@@ -79,192 +80,192 @@ module top
                     .pwm_0_on_i(register_led_w[(0*32)+11]),
                     .pwm_0_off_i(register_led_w[(0*32)+27]),
                     .pwm_0_high_i({
-                                   {register_led_w[(0*32)+12:(0*32)+15]},
-                                   {register_led_w[(0*32)+0:(0*32)+7]}
+                                   {register_led_w[(0*32)+12 +: 4]},
+                                   {register_led_w[(0*32) +: 8]}
                                    }),
                     .pwm_0_low_i({
-                                  {register_led_w[(0*32)+28:(0*32)+31]},
-                                  {register_led_w[(0*32)+16:(0*32)+23]}
+                                  {register_led_w[(0*32)+28 +: 4]},
+                                  {register_led_w[(0*32)+16 +: 8]}
                                   }),
                     .pwm_0_o(pwm_w[0]),
 
                     .pwm_1_on_i(register_led_w[(1*32)+11]),
                     .pwm_1_off_i(register_led_w[(1*32)+27]),
                     .pwm_1_high_i({
-                                   {register_led_w[(1*32)+12:(1*32)+15]},
-                                   {register_led_w[(1*32)+0:(1*32)+7]}
+                                   {register_led_w[(1*32)+12 +: 4]},
+                                   {register_led_w[(1*32) +: 8]}
                                    }),
                     .pwm_1_low_i({
-                                  {register_led_w[(1*32)+28:(1*32)+31]},
-                                  {register_led_w[(1*32)+16:(1*32)+23]}
+                                  {register_led_w[(1*32)+28 +: 4]},
+                                  {register_led_w[(1*32)+16 +: 8]}
                                   }),
                     .pwm_1_o(pwm_w[1]),
 
                     .pwm_2_on_i(register_led_w[(2*32)+11]),
                     .pwm_2_off_i(register_led_w[(2*32)+27]),
                     .pwm_2_high_i({
-                                   {register_led_w[(2*32)+12:(2*32)+15]},
-                                   {register_led_w[(2*32)+0:(2*32)+7]}
+                                   {register_led_w[(2*32)+12 +: 4]},
+                                   {register_led_w[(2*32) +: 8]}
                                    }),
                     .pwm_2_low_i({
-                                  {register_led_w[(2*32)+28:(2*32)+31]},
-                                  {register_led_w[(2*32)+16:(2*32)+23]}
+                                  {register_led_w[(2*32)+28 +: 4]},
+                                  {register_led_w[(2*32)+16 +: 8]}
                                   }),
                     .pwm_2_o(pwm_w[2]),
 
                     .pwm_3_on_i(register_led_w[(3*32)+11]),
                     .pwm_3_off_i(register_led_w[(3*32)+27]),
                     .pwm_3_high_i({
-                                   {register_led_w[(3*32)+12:(3*32)+15]},
-                                   {register_led_w[(3*32)+0:(3*32)+7]}
+                                   {register_led_w[(3*32)+12 +: 4]},
+                                   {register_led_w[(3*32) +: 8]}
                                    }),
                     .pwm_3_low_i({
-                                  {register_led_w[(3*32)+28:(3*32)+31]},
-                                  {register_led_w[(3*32)+16:(3*32)+23]}
+                                  {register_led_w[(3*32)+28 +: 4]},
+                                  {register_led_w[(3*32)+16 +: 8]}
                                   }),
                     .pwm_3_o(pwm_w[3]),
 
                     .pwm_4_on_i(register_led_w[(4*32)+11]),
                     .pwm_4_off_i(register_led_w[(4*32)+27]),
                     .pwm_4_high_i({
-                                   {register_led_w[(4*32)+12:(4*32)+15]},
-                                   {register_led_w[(4*32)+0:(4*32)+7]}
+                                   {register_led_w[(4*32)+12 +: 4]},
+                                   {register_led_w[(4*32) +: 8]}
                                    }),
                     .pwm_4_low_i({
-                                  {register_led_w[(4*32)+28:(4*32)+31]},
-                                  {register_led_w[(4*32)+16:(4*32)+23]}
+                                  {register_led_w[(4*32)+28 +: 4]},
+                                  {register_led_w[(4*32)+16 +: 8]}
                                   }),
                     .pwm_4_o(pwm_w[4]),
 
                     .pwm_5_on_i(register_led_w[(5*32)+11]),
                     .pwm_5_off_i(register_led_w[(5*32)+27]),
                     .pwm_5_high_i({
-                                   {register_led_w[(5*32)+12:(5*32)+15]},
-                                   {register_led_w[(5*32)+0:(5*32)+7]}
+                                   {register_led_w[(5*32)+12 +: 4]},
+                                   {register_led_w[(5*32) +: 8]}
                                    }),
                     .pwm_5_low_i({
-                                  {register_led_w[(5*32)+28:(5*32)+31]},
-                                  {register_led_w[(5*32)+16:(5*32)+23]}
+                                  {register_led_w[(5*32)+28 +: 4]},
+                                  {register_led_w[(5*32)+16 +: 8]}
                                   }),
                     .pwm_5_o(pwm_w[5]),
 
                     .pwm_6_on_i(register_led_w[(6*32)+11]),
                     .pwm_6_off_i(register_led_w[(6*32)+27]),
                     .pwm_6_high_i({
-                                   {register_led_w[(6*32)+12:(6*32)+15]},
-                                   {register_led_w[(6*32)+0:(6*32)+7]}
+                                   {register_led_w[(6*32)+12 +: 4]},
+                                   {register_led_w[(6*32) +: 8]}
                                    }),
                     .pwm_6_low_i({
-                                  {register_led_w[(6*32)+28:(6*32)+31]},
-                                  {register_led_w[(6*32)+16:(6*32)+23]}
+                                  {register_led_w[(6*32)+28 +: 4]},
+                                  {register_led_w[(6*32)+16 +: 8]}
                                   }),
                     .pwm_6_o(pwm_w[6]),
 
                     .pwm_7_on_i(register_led_w[(7*32)+11]),
                     .pwm_7_off_i(register_led_w[(7*32)+27]),
                     .pwm_7_high_i({
-                                   {register_led_w[(7*32)+12:(7*32)+15]},
-                                   {register_led_w[(7*32)+0:(7*32)+7]}
+                                   {register_led_w[(7*32)+12 +:4]},
+                                   {register_led_w[(7*32) +: 8]}
                                    }),
                     .pwm_7_low_i({
-                                  {register_led_w[(7*32)+28:(7*32)+31]},
-                                  {register_led_w[(7*32)+16:(7*32)+23]}
+                                  {register_led_w[(7*32)+28 +: 4]},
+                                  {register_led_w[(7*32)+16 +: 8]}
                                   }),
                     .pwm_7_o(pwm_w[7]),
 
                     .pwm_8_on_i(register_led_w[(8*32)+11]),
                     .pwm_8_off_i(register_led_w[(8*32)+27]),
                     .pwm_8_high_i({
-                                   {register_led_w[(8*32)+12:(8*32)+15]},
-                                   {register_led_w[(8*32)+0:(8*32)+7]}
+                                   {register_led_w[(8*32)+12 +: 4]},
+                                   {register_led_w[(8*32) +: 8]}
                                    }),
                     .pwm_8_low_i({
-                                  {register_led_w[(8*32)+28:(8*32)+31]},
-                                  {register_led_w[(8*32)+16:(8*32)+23]}
+                                  {register_led_w[(8*32)+28 +: 4]},
+                                  {register_led_w[(8*32)+16 +: 8]}
                                   }),
                     .pwm_8_o(pwm_w[8]),
 
                     .pwm_9_on_i(register_led_w[(9*32)+11]),
                     .pwm_9_off_i(register_led_w[(9*32)+27]),
                     .pwm_9_high_i({
-                                   {register_led_w[(9*32)+12:(9*32)+15]},
-                                   {register_led_w[(9*32)+0:(9*32)+7]}
+                                   {register_led_w[(9*32)+12 +: 4]},
+                                   {register_led_w[(9*32) +: 8]}
                                    }),
                     .pwm_9_low_i({
-                                  {register_led_w[(9*32)+28:(9*32)+31]},
-                                  {register_led_w[(9*32)+16:(9*32)+23]}
+                                  {register_led_w[(9*32)+28 +: 4]},
+                                  {register_led_w[(9*32)+16 +: 8]}
                                   }),
                     .pwm_9_o(pwm_w[9]),
 
                     .pwm_10_on_i(register_led_w[(10*32)+11]),
                     .pwm_10_off_i(register_led_w[(10*32)+27]),
                     .pwm_10_high_i({
-                                   {register_led_w[(10*32)+12:(10*32)+15]},
-                                   {register_led_w[(10*32)+0:(10*32)+7]}
+                                   {register_led_w[(10*32)+12 +: 4]},
+                                   {register_led_w[(10*32) +: 8]}
                                    }),
                     .pwm_10_low_i({
-                                  {register_led_w[(10*32)+28:(10*32)+31]},
-                                  {register_led_w[(10*32)+16:(10*32)+23]}
+                                  {register_led_w[(10*32)+28 +: 4]},
+                                  {register_led_w[(10*32)+16 +:8]}
                                   }),
                     .pwm_10_o(pwm_w[10]),
 
                     .pwm_11_on_i(register_led_w[(11*32)+11]),
                     .pwm_11_off_i(register_led_w[(11*32)+27]),
                     .pwm_11_high_i({
-                                   {register_led_w[(11*32)+12:(11*32)+15]},
-                                   {register_led_w[(11*32)+0:(11*32)+7]}
+                                   {register_led_w[(11*32)+12 +: 4]},
+                                   {register_led_w[(11*32) +: 8]}
                                    }),
                     .pwm_11_low_i({
-                                  {register_led_w[(11*32)+28:(11*32)+31]},
-                                  {register_led_w[(11*32)+16:(11*32)+23]}
+                                  {register_led_w[(11*32)+28 +: 4]},
+                                  {register_led_w[(11*32)+16 +: 8]}
                                   }),
                     .pwm_11_o(pwm_w[11]),
 
                     .pwm_12_on_i(register_led_w[(12*32)+11]),
                     .pwm_12_off_i(register_led_w[(12*32)+27]),
                     .pwm_12_high_i({
-                                   {register_led_w[(12*32)+12:(12*32)+15]},
-                                   {register_led_w[(12*32)+0:(12*32)+7]}
+                                   {register_led_w[(12*32)+12 +: 4]},
+                                   {register_led_w[(12*32) +: 8]}
                                    }),
                     .pwm_12_low_i({
-                                  {register_led_w[(12*32)+28:(12*32)+31]},
-                                  {register_led_w[(12*32)+16:(12*32)+23]}
+                                  {register_led_w[(12*32)+28 +: 4]},
+                                  {register_led_w[(12*32)+16 +: 8]}
                                   }),
                     .pwm_12_o(pwm_w[12]),
 
                     .pwm_13_on_i(register_led_w[(13*32)+11]),
                     .pwm_13_off_i(register_led_w[(13*32)+27]),
                     .pwm_13_high_i({
-                                   {register_led_w[(13*32)+12:(13*32)+15]},
-                                   {register_led_w[(13*32)+0:(13*32)+7]}
+                                   {register_led_w[(13*32)+12 +: 4]},
+                                   {register_led_w[(13*32) +: 8]}
                                    }),
                     .pwm_13_low_i({
-                                  {register_led_w[(13*32)+28:(13*32)+31]},
-                                  {register_led_w[(13*32)+16:(13*32)+23]}
+                                  {register_led_w[(13*32)+28 +: 4]},
+                                  {register_led_w[(13*32)+16 +: 8]}
                                   }),
                     .pwm_13_o(pwm_w[13]),
 
                     .pwm_14_on_i(register_led_w[(14*32)+11]),
                     .pwm_14_off_i(register_led_w[(14*32)+27]),
                     .pwm_14_high_i({
-                                   {register_led_w[(14*32)+12:(14*32)+15]},
-                                   {register_led_w[(14*32)+0:(14*32)+7]}
+                                   {register_led_w[(14*32)+12 +: 4]},
+                                   {register_led_w[(14*32) +: 8]}
                                    }),
                     .pwm_14_low_i({
-                                  {register_led_w[(14*32)+28:(14*32)+31]},
-                                  {register_led_w[(14*32)+16:(14*32)+23]}
+                                  {register_led_w[(14*32)+28 +: 4]},
+                                  {register_led_w[(14*32)+16 +: 8]}
                                   }),
                     .pwm_14_o(pwm_w[14]),
 
                     .pwm_15_on_i(register_led_w[(15*32)+11]),
                     .pwm_15_off_i(register_led_w[(15*32)+27]),
                     .pwm_15_high_i({
-                                   {register_led_w[(15*32)+12:(15*32)+15]},
-                                   {register_led_w[(15*32)+0:(15*32)+7]}
+                                   {register_led_w[(15*32)+12 +: 4]},
+                                   {register_led_w[(15*32) +: 8]}
                                    }),
                     .pwm_15_low_i({
-                                  {register_led_w[(15*32)+28:(15*32)+31]},
-                                  {register_led_w[(15*32)+16:(15*32)+23]}
+                                  {register_led_w[(15*32)+28 +: 4]},
+                                  {register_led_w[(15*32)+16 +:8]}
                                   }),
                     .pwm_15_o(pwm_w[15])
                     
